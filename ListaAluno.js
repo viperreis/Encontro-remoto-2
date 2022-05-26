ee1b8f7cabc35949d4b97578573b3a29b25d1899
unknown <victor.per.reis@gmail.com>
2022-05-25 21:14:02 -0300

    Exercícios do encontro remoto 2

---
+++ ListaAluno.js
@@ -0,0 +1,60 @@
+let listaalunos = ["Victor","Thiago","Lúcia","Giovanna"];
+
+for (let contador = 0; contador <= listaalunos.length; contador++)
+    
+    if (contador == 0) {
+       console.log ("o número atual é zero")
+    
+        //concatenação
+    } else if(contador % 2 == 0){
+        console.log (`o número ${contador} é par`)
+    
+    }else{
+        console.log(`o número ${contador} é impar`)}
+    
+        for (let contador = 0; contador <= listaalunos.length; contador++)
+        for (let nome of listaalunos) {
+        console.log(`esta pessoa se chama ${nome} e sua posição é ` + contador++)}
+// Isto foi o máximo que consegui
+
+
+
+//Lista == forit ou forof(javascript)
+
+// lista com quantidade == for
+
+// condição (escolha que não é quantidade) == while
+
+//     if (contador == 0) {
+//         console.log ("o número atual é zero")
+    
+//         //interpolação
+//     }else if(contador % 2 == 0){
+//         console.log (`o número ${contador} é par`)
+    
+//     }else{
+//         console.log(`o número ${contador} é impar`)
+
+//     }
+// }
+
+// let contador = 0
+
+// while (contador < numeroalunos) {
+    
+//     if (contador == 0) {
+//         console.log ("o número atual é zero")
+    
+//         //interpolação
+//     }else if(contador % 2 == 0){
+//         console.log (`o número ${contador} é par`)
+    
+//     }else{
+//         console.log(`o número ${contador} é impar`)
+
+
+//     }
+
+
+//     contador++
+
